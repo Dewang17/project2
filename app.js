a4a2915a078039685b8f951a6a956f236c4d974e
Dewang sankadiya <dewangsankadiya9@gmail.com>
2025-02-18 08:51:44 +0530

    intia  commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let h1=document.querySelector("h1");
+function name(name){
+    return name;
+}
+ h1.addEventListener("mouseenter",()=>{
+    console.log(`the user name:${username} vistied to the point1`)
+ });
