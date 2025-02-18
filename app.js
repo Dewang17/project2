6c0977543d248d5ff5f59e6996f484679fb49ecf
Dewang sankadiya <dewangsankadiya9@gmail.com>
2025-02-18 09:08:12 +0530

    up todate

---
+++ app.js
@@ -0,0 +1,8 @@
+let h1=document.querySelector("h1");
+function name(name){
+    return name;
+}
+ h1.addEventListener("mouseenter",()=>{
+    console.log(`the user name:${username} vistied to the point1`)
+ });
+ 
